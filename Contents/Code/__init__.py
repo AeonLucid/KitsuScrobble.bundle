a3cec274b7099697fe54ec8a3efa238e0322297f
AeonLucid <aeonlucid@outlook.com>
2016-12-05 10:05:39 +0100

    Initial.

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,9 @@
+PLUGIN_NAME = "HummingBirdScrobbler"
+
+
+def Start():
+    Log.Info("[%s] Starting HummingBirdScrobbler." % PLUGIN_NAME)
+
+
+def ValidatePrefs():
+    Log.Info("[%s] Nothing to validate for HummingBirdScrobbler" % PLUGIN_NAME)
