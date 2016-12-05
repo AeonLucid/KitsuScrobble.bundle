cc130950912a3a0546a52fc7c192c7f63fa095e3
AeonLucid <aeonlucid@outlook.com>
2016-12-05 17:14:06 +0100

    Changed name and quotes.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,5 @@
-PLUGIN_NAME = "HummingBirdScrobbler"
+PLUGIN_NAME = "HBScrobbler"
+ROUTE_BASE = PLUGIN_NAME.lower()
 
 API_AUTHENTICATE_URL = "http://hummingbird.me/api/v1/users/authenticate"
 
@@ -21,31 +22,60 @@ def ValidatePrefs():
 
 ####################################################################################################
 def Authenticate():
-    Log.Debug("Attempting HummingBird authentication")
+    Log.Debug("Attempting HummingBird authentication.")
 
-    if Prefs['username'] and Prefs['password']:
+    if Prefs["username"] and Prefs["password"]:
         try:
             authToken = JSON.ObjectFromURL(API_AUTHENTICATE_URL, values=dict(
-                username=Prefs['username'],
-                password=Prefs['password']))
+                username=Prefs["username"],
+                password=Prefs["password"]))
 
-            Dict['logged_in'] = True
-            Dict['auth_token'] = authToken
+            Dict["logged_in"] = True
+            Dict["auth_token"] = authToken
             Log.Info("[%s] Authentication was successful." % PLUGIN_NAME)
             return True
         except Ex.HTTPError, e:
-            Dict['logged_in'] = False
+            Dict["logged_in"] = False
             Log.Error("[%s] %s" % (PLUGIN_NAME, e.content))
             Log.Error("[%s] Authentication failed." % PLUGIN_NAME)
         except:
-            Dict['logged_in'] = False
+            Dict["logged_in"] = False
             Log.Error("[%s] Authentication failed." % PLUGIN_NAME)
     return False
 
 
 ####################################################################################################
-@handler('/video/hummingbirdscrobbler', PLUGIN_NAME)
+@handler("/video/%s" % ROUTE_BASE, PLUGIN_NAME)
 def MainMenu():
-    oc = ObjectContainer(no_cache=True)
-    oc.add(PrefsObject(title="Preferences"))
-    return oc
+    return ObjectContainer(
+        no_cache=True,
+        no_history=True,
+        objects=[
+            DirectoryObject(
+                key=Callback(LookupUnmatchedAnime),
+                title="Lookup unmatched anime"
+            ),
+            PrefsObject(title="Preferences")
+        ]
+    )
+
+
+@route("/video/%s/lookup-unmatched-anime" % ROUTE_BASE)
+def LookupUnmatchedAnime():
+    if not Dict["logged_in"]:
+        return ObjectContainer(header="Login", message="Enter your username and password in the %s preferences" % PLUGIN_NAME)
+
+    # response = JSON.ObjectFromURL(request_url)
+    # collection = response["collection"]
+    
+    # oc = ObjectContainer(title2 = "My Stream")
+    # for activity in collection:
+    #     origin = activity["origin"]
+    #     if not origin["streamable"] or "stream_url" not in origin:
+    #         continue
+    #     AddTrack(oc, origin)
+    
+    # if "next_href" in response:
+    #     next_href = response["next_href"]
+
+    return ObjectContainer(header="Login Failed", message="Hello")
